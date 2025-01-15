5dc0f8edeafd837b5ce017b282118e78612efb5b
Matheus Compagnoni <compagnoni_santos@hotmail.com>
2025-01-14 21:12:24 -0300

    adicionando a informação de seguidores e seguindo do usuário pesquisado, ainda incompleto

--- src/scripts/objects/user.js
+++ src/scripts/objects/user.js
@@ -3,12 +3,16 @@ const user = {
     name: '',
     bio: '',
     userName: '',
+    followers: '',
+    following: '',
     repositories: [],
     setInfo(gitHubUser){
         this.avatarUrl = gitHubUser.avatar_url
         this.name = gitHubUser.name
         this.bio = gitHubUser.bio
         this.userName = gitHubUser.login
+        this.followers = gitHubUser.followers_url
+        this.following = gitHubUser.following_url
     },
     setRepositories(repositories){
         this.repositories = repositories
